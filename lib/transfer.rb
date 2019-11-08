5aeda43d263b20bf8fc72ba7fc57d1e0cc1dba50
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-08 20:59:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,11 @@ class Transfer
  end
  
  def reverse_transfer
-   
+   if status == "complete"
+   @sender.balance += @amount
+    @receiver.balance -= @amount
+    @status = "reversed"
+   end
  end
  
 end
